5b9a5b9b463bce1f98e2da65ab4e139b9bde1825
Meskerem Fekede <149380795+meskeremfekede@users.noreply.github.com>
2025-02-12 02:17:47 +0300

    Update counter.js

--- task-three/src/counter.js
+++ task-three/src/counter.js
@@ -1,1 +1,20 @@
+import { useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    const Increment = () => setCount(prevCount => prevCount + 1);
+    const Decrement = () => setCount(prevCount => prevCount - 1);
+    const Reset = () => setCount(0);
+
+    return (
+        <div className="counter">
+            <h2>Counter App</h2>
+            <p>{count}</p>
+            <button onClick={Increment}>+</button>
+            <button onClick={Decrement} disabled={count === 0}>-</button>
+            <button onClick={Reset}>Reset</button>
+        </div>
+    );
+}
 
