52ad788fcab553b863695838002db9b819d678fd
Meskerem Fekede <149380795+meskeremfekede@users.noreply.github.com>
2025-02-27 03:27:05 +0300

    Create App.jsx

---
+++ task4/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FAQAccordion from "./components/FAQAccordion";
+import "./components/FAQAccordion.css";
+
+function App() {
+  return (
+    <div>
+      <h1>FAQ Section</h1>
+      <FAQAccordion />
+    </div>
+  );
+}
+
+export default App;
